99d314e89032a133f8dd6e7b9fad7619bd1daa8b
kuoran <nicky2153@nate.com>
2014-03-27 23:26:21 +0900

    how to use

--- src/test3/first/AAA2.java
+++ src/test3/first/AAA2.java
@@ -4,6 +4,9 @@ public class AAA2 {
 	public static void main(String[] args) {
 		System.out.println("ㅠㅠ");
 		System.out.println("PCPCPCPCPCPCP");
+		//가지에다가 막 한다
+		//에러가 없다
+		//local branches에 있는 개인용?새로만든?가지를 커밋한다
 	}
 
 }
